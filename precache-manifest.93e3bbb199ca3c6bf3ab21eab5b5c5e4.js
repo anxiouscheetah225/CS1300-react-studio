c4d8ee7c315f2e928fb1b08cf3cd5e34b21460d8
is-ali25 <jabril_muhammad@brown.edu>
2022-12-16 02:00:10 -0500

    Updates

---
+++ precache-manifest.93e3bbb199ca3c6bf3ab21eab5b5c5e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b28ec5cede7246dee6d9",
+    "url": "/CS1300-react-studio/static/css/main.38b751c8.chunk.css"
+  },
+  {
+    "revision": "b28ec5cede7246dee6d9",
+    "url": "/CS1300-react-studio/static/js/main.702e07c9.chunk.js"
+  },
+  {
+    "revision": "3ada0c10562f1bd3c335",
+    "url": "/CS1300-react-studio/static/js/runtime~main.c59c6e27.js"
+  },
+  {
+    "revision": "024e0569cd0f5834eee7",
+    "url": "/CS1300-react-studio/static/js/2.0710097d.chunk.js"
+  },
+  {
+    "revision": "ce165983f250aaa94dda",
+    "url": "/CS1300-react-studio/static/js/3.49a205bf.chunk.js"
+  },
+  {
+    "revision": "d688f7f77fbfeb3cc2cdc1499c0702ea",
+    "url": "/CS1300-react-studio/index.html"
+  }
+];
